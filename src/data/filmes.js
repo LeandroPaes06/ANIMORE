75d2da43c20876233efc0520a7fba58c2aab6c7d
LeandroPaes06 <leandrinpaes@gmail.com>
2023-06-02 03:12:12 -0300

    Animore

---
+++ src/data/filmes.js
@@ -0,0 +1,60 @@
+const DATA = 
+[
+{
+    id:'01',
+    nome:'Dog 1',
+    imagem:'dog1.png'
+},
+{
+    id: '02',
+    nome: 'Dog 2',
+    imagem:'dog2.png',
+},
+{
+    id: '03',
+    nome: 'Dog 3',
+    imagem:'dog3.png'
+},
+{
+    id: '04',
+    nome:'Dog 4',
+    imagem:'dog4.png'
+},
+{
+    id: '05',
+    nome:'Dog 5',
+    imagem:'dog5.png'
+},
+{
+    id: '06',
+    nome:'Dog 6',
+    imagem:'dog6.png'
+},
+{
+    id: '07',
+    nome:'Dog 7',
+    imagem:'dog7.png'
+},
+{
+    id: '08',
+    nome:'Dog 8',
+    imagem:'dog8.png'
+},
+{
+    id: '09',
+    nome:'Dog 9',
+    imagem:'dog9.png'
+},
+{
+    id: '10',
+    nome:'Cat 10',
+    imagem:'cat10.png'
+},
+{
+    id: '11',
+    nome:'Dog 11',
+    imagem:'dog11.png'
+},
+]
+
+export default DATA;
